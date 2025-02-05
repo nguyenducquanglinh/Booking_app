9212e63aaf1d6f372acaaf436b0f65432498868c
nguyenducquanglinh <linh35889@gmail.com>
2025-02-05 14:20:42 +0700

    search

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -14,8 +14,14 @@ const List = () => {
   const [date, setDate] = useState(location.state.date);
   const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
+  const [min, setMin] = useState(undefined);
+  const [max, setMax] = useState(undefined);
 
-  const { data, loading, error, refetch } = useFetch(`/hotel?city=${destination}`)
+  const { data, loading, error, reFetch } = useFetch(`/hotels?city=${destination}&min=${min || 0}&max=${max || 999}`)
+
+  const handleClick = () => {
+    reFetch()
+  }
 
   return (
     <div>
@@ -50,13 +56,13 @@ const List = () => {
                   <span className="lsOptionText">
                     Min price <small>per night</small>
                   </span>
-                  <input type="number" className="lsOptionInput" />
+                  <input type="number" onChange={e => setMin(e.target.value)} className="lsOptionInput" />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">
                     Max price <small>per night</small>
                   </span>
-                  <input type="number" className="lsOptionInput" />
+                  <input type="number" onChange={e => setMax(e.target.value)} className="lsOptionInput" />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">Adult</span>
@@ -87,14 +93,14 @@ const List = () => {
                 </div>
               </div>
             </div>
-            <button>Search</button>
+            <button onClick={handleClick}>Search</button>
           </div>
           <div className="listResult">
-            {loading ? "loading":<>
-           {data.map(item=>(
-            <SearchItem item={item} key={item._id} />
-            ))}
-             </>}
+            {loading ? "loading" : <>
+              {data.map(item => (
+                <SearchItem item={item} key={item._id} />
+              ))}
+            </>}
           </div>
         </div>
       </div>
